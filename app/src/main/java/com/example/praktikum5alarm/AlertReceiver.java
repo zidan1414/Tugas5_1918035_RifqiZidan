ef81759943ce4bfbb521e13b4f9c0b7fd0d97f0e
unknown <rifqizidane1999@gmail.com>
2021-11-07 20:44:36 +0700

    first commit

---
+++ app/src/main/java/com/example/praktikum5alarm/AlertReceiver.java
@@ -0,0 +1,20 @@
+package com.example.praktikum5alarm;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.core.app.NotificationCompat;
+
+public class AlertReceiver extends BroadcastReceiver {
+    //ketika menerima broadcast dari alarm, maka akan segera memunculkan notifikasi
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        NotificationHelper notificationHelper = new
+                NotificationHelper(context);
+        NotificationCompat.Builder nb =
+                notificationHelper.getChannelNotification();
+        notificationHelper.getManager().notify(1, nb.build());
+    }
+
+}
